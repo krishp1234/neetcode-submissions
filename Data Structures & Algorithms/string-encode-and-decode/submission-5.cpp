63004e72a4125df19bacb8199e147b43c235e310
krishp1234 <krishspatel19@gmail.com>
2026-07-15 16:28:11 -0500

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded_string = "";
+        for(int i = 0; i < strs.size(); ++i){
+            encoded_string += to_string(strs[i].size()) + '#' + strs[i];
+        }
+        return encoded_string;
+    }
+    //4#neetcode5#code
+    vector<string> decode(string s) {
+        vector<string> decoder;
+        int i = 0;
+        while(i < s.size()){
+            int j = i;
+            while(s[j] != '#'){
+                ++j;
+            }
+            int length = stoi(s.substr(i, j - i));
+            decoder.push_back(s.substr(j + 1, length));
+            i = j + 1 + length;
+        }
+        return decoder;
+
+    }
+};
