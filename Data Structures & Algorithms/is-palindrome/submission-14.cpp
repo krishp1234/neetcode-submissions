b26d1090351a3a2a87166908cde9e6080d75633e
krishp1234 <krishspatel19@gmail.com>
2026-07-14 20:14:41 -0500

    Add: is-palindrome - submission-14

---
+++ Data Structures & Algorithms/is-palindrome/submission-14.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string cleaned = "";
+        for(auto i : s){
+            if(isalnum(i)){
+                cleaned += tolower(i);
+            }
+        }
+        if(cleaned.empty()){
+            return true;
+        }
+        //NooN radar
+        auto start = cleaned.begin();
+        auto end = cleaned.end() - 1;
+
+        while(start < end){
+            if(*start != *end){
+                return false;
+            }
+            ++start;
+            --end;
+        }
+        return true;
+
+    }
+};
