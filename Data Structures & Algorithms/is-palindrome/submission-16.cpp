a1caf7901f935d5531ed6abd42b1f7e1689289e4
krishp1234 <krishspatel19@gmail.com>
2026-08-01 11:33:19 -0500

    Add: is-palindrome - submission-16

---
+++ Data Structures & Algorithms/is-palindrome/submission-16.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string cleaned = "";
+        for(auto i : s){
+            if(isalnum(i)){
+                cleaned += tolower(i);
+            }
+        }
+        
+        int start = 0;
+        int end = cleaned.size() - 1;
+        while(start < end){
+            if(cleaned[start] != cleaned[end]){
+                return false;
+            }
+            ++start;
+            --end;
+        }
+        return true;
+    }
+};
