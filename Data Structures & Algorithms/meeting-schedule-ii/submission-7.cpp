7921dea041b0f416a000d883616d6a0e36e79b10
krishp1234 <krishspatel19@gmail.com>
2026-08-18 09:13:05 -0500

    Add: meeting-schedule-ii - submission-7

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-7.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+bool comparator(const Interval &a, const Interval &b){
+        return a.start < b.start;
+    }
+class Solution {
+    
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), comparator);
+        priority_queue<int, vector<int>, greater<int>> end_times;
+
+        for(auto i : intervals){
+            if(!end_times.empty() && end_times.top() <= i.start){
+                end_times.pop();
+            }
+            end_times.push(i.end);
+        }
+        return end_times.size();
+
+    }
+};
