7c4ba3a78b9dc9597934a8dc3bc98818e0726181
krishp1234 <krishspatel19@gmail.com>
2026-08-30 15:08:25 -0500

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<string> current;
+        vector<vector<string>> final;
+        backtrack(s, 0, current, final);
+        return final;
+    }
+    void backtrack(string s, int i, vector<string>& current,
+    vector<vector<string>>& final){
+        
+        if(i == s.size()){
+            final.push_back(current);
+            return;
+        }
+        for(int j = i; j < s.size(); ++j){
+            if(palindrome(s, i, j)){
+                current.push_back(s.substr(i, j - i + 1));
+                backtrack(s, j + 1, current, final);
+                current.pop_back();
+            }
+        }
+
+    }
+    bool palindrome(string s, int i, int j){
+        while(i < j){
+            if(s[i] != s[j]){
+                return false;
+            }
+            ++i;
+            --j;
+        }
+        return true;
+    }
+};
