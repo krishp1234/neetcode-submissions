99dad59d5183aca5cde66f9ec66fbb4c012ecd7c
krishp1234 <krishspatel19@gmail.com>
2026-08-30 12:21:40 -0500

    Add: subsets-ii - submission-3

---
+++ Data Structures & Algorithms/subsets-ii/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> combs;
+        vector<int> current;
+        backtracking(nums, 0, combs, current);
+        return combs;
+    }
+    void backtracking(vector<int> &nums, int i, 
+    vector<vector<int>> &combs, vector<int> &current){
+        if(i == nums.size()){
+            combs.push_back(current);
+            return;
+        }
+        current.push_back(nums[i]);
+        backtracking(nums, i + 1, combs, current);
+
+        current.pop_back();
+        while(i + 1 < nums.size() && nums[i] == nums[i + 1]){
+            ++i;
+        }
+        backtracking(nums, i + 1, combs, current);
+    }
+};
