a84f424f47a56e183044432f882fe9ff1b5cca79
krishp1234 <krishspatel19@gmail.com>
2026-09-23 10:44:06 -0500

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+       unordered_map<int, int> duplicates;
+       for(int i = 0; i < nums.size(); ++i){
+        if(duplicates.contains(nums[i]) && abs(duplicates[nums[i]] - i) <= k){
+            return true;
+            
+        }
+        if(duplicates.contains(nums[i]) && abs(duplicates[nums[i]] - i) > k){
+            duplicates.erase(nums[i]);
+        }
+        duplicates.insert({nums[i], i});
+       } 
+       return false;
+    }
+};
