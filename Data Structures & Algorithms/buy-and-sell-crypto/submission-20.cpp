1e45d8d93dabbfdec73b7800606990b9c520d839
krishp1234 <krishspatel19@gmail.com>
2026-09-24 14:50:40 -0500

    Add: buy-and-sell-crypto - submission-20

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-20.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+       int buy = 0;
+       int sell = 0;
+       int profit = 0;
+
+       while(sell < prices.size()){
+        if(prices[buy] >= prices[sell]){
+            buy = sell;
+        }
+        profit = max(profit, prices[sell] - prices[buy]);
+        ++sell;
+       } 
+       return profit;
+    }
+};
